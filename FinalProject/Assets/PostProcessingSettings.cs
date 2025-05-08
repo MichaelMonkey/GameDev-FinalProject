4b9683bc09fe8cc964dacf96f92cf8902af3e6d6
MichaelMonkey <michaelmonkey0128@gmail.com>
2025-05-07 22:42:16 -0500

    Added additional Options
    Added options for full screen, vsync, brightness, and recolor in the options menu.

--- FinalProject/Assets/PostProcessingSettings.cs
+++ FinalProject/Assets/PostProcessingSettings.cs
@@ -7,6 +7,10 @@ public class PostProcessingSettings : MonoBehaviour
 {
     public Volume volume;
     public Slider contrastSlider;
+    public Slider brightnessSlider;
+
+    public Slider recolorSlider;
+
     ColorAdjustments colorAdjustments;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +19,8 @@ public class PostProcessingSettings : MonoBehaviour
             Debug.LogError("No color adjustments found");
         }
         contrastSlider.value = PlayerPrefs.GetFloat("contrast", 0f);
+        brightnessSlider.value = PlayerPrefs.GetFloat("brightness", 0f);
+        recolorSlider.value = PlayerPrefs.GetFloat("recolor", 0f);
     }
 
     // Update is called once per frame
@@ -24,6 +30,14 @@ public class PostProcessingSettings : MonoBehaviour
 
     public void SetContrast(){
         colorAdjustments.contrast.value = contrastSlider.value;
-        PlayerPrefs.SetFloat("contrast", contrastSlider.value);
+        PlayerPrefs.SetFloat("contrast", contrastSlider.value); 
+    }
+    public void SetBrightness(){
+        colorAdjustments.postExposure.value = brightnessSlider.value;
+        PlayerPrefs.SetFloat("brightness", brightnessSlider.value); 
+    }
+    public void SetRecolor(){
+        colorAdjustments.hueShift.value = recolorSlider.value;
+        PlayerPrefs.SetFloat("recolor", recolorSlider.value); 
     }
 }
--- FinalProject/Assets/ScreenSettings.cs
+++ FinalProject/Assets/ScreenSettings.cs
@@ -8,6 +8,8 @@ using System;
 public class ScreenSettings : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle;
+    public Toggle vsyncToggle;
     Resolution[] resolutions;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,6 +28,16 @@ public class ScreenSettings : MonoBehaviour
             resolutionDropdown.options.Add(newOption);
         }
         resolutionDropdown.value = currentIndex;
+
+        int fullscreenStatus = PlayerPrefs.GetInt("fullscreen", 0);
+        bool isFullscreen = (fullscreenStatus == 1);
+        fullscreenToggle.isOn = isFullscreen;
+        SetFullscreen(isFullscreen);
+
+        int vSyncCount = PlayerPrefs.GetInt("vsync", 1);
+        bool isVsync = (vSyncCount == 1); 
+        vsyncToggle.isOn = isVsync;
+        SetVsync(isVsync);
     }
     public void SetResolution(){
         int currentIndex = resolutionDropdown.value;
@@ -34,6 +46,33 @@ public class ScreenSettings : MonoBehaviour
         PlayerPrefs.SetInt("resolution", currentIndex);
     }
 
+    public void SetFullscreen(bool isFullscreen){
+        Screen.fullScreen = isFullscreen;
+        int fullscreenStatus = 0;
+        if(isFullscreen){
+            fullscreenStatus = 1;
+        }
+        PlayerPrefs.SetInt("fullscreen", fullscreenStatus);
+        if(isFullscreen){
+            print("Enabled fullscreen");
+        } else {
+            print("Disabled fullscreen");
+        }
+    }
+
+    public void SetVsync(bool isVsync){
+        int vSyncCount = 0;
+        if(isVsync){
+            vSyncCount = 1;
+        }
+        QualitySettings.vSyncCount = vSyncCount;
+        PlayerPrefs.SetInt("vsync", vSyncCount);
+        if(isVsync){
+            print("Enabled vsync");
+        } else {
+            print("Disabled vsync");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
